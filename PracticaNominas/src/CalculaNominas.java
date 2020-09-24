1121a24eda766d55c6521b607b566e820dd197c0
Ryu <miguelespigaresrecio@iessoterohernandez.es>
2020-09-24 09:12:34 +0200

    Monchito

--- PracticaNominas/src/CalculaNominas.java
+++ PracticaNominas/src/CalculaNominas.java
@@ -1,9 +1,13 @@
+import java.util.ArrayList;
+
 public class CalculaNominas {
     public static void main(String[] args) {
         GestionBBDD gestionBBDD = new GestionBBDD();
+        ModificarFichero modificarFichero = new ModificarFichero();
         Empleado empleado1 = null;
         Empleado empleado2 = null;
         Empleado empleado3=null;
+        ArrayList<Empleado> empleados = new ArrayList<>();
         try {
             empleado1 = new Empleado("James Cosling", "32000032G", 'M', 4, 7);
             empleado3 = new Empleado("James Cosling", "32000032P", 'M', 4, 7);
@@ -13,7 +17,7 @@ public class CalculaNominas {
         } catch (DatosNoCorrectosException e) {
             System.out.println("Introduce los datos correctamente");
         }
-
+/*
         if (empleado1 != null && empleado2 != null) {
             gestionBBDD.actualizarCategoria(empleado2,9);
             gestionBBDD.actualizarSueldo(empleado2);
@@ -21,8 +25,12 @@ public class CalculaNominas {
             gestionBBDD.insertarEmpleado(empleado3);
 
         }
+*/
+        empleados.add(empleado1);
+        empleados.add(empleado2);
+        empleados.add(empleado3);
 
-
+        modificarFichero.escribirFichero(empleados);
     }
 /*
     private static void escribe(Empleado empleado1, Empleado empleado2) {
